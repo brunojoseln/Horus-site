d845a01c8dab3af56418fc6e866de47cf101ff48
Thiago Neves <thiago.sobrinho@etec.sp.gov.br>
2018-08-06 13:19:14 -0300

    Including all files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function(){
+  $(window).scroll(function(){
+    var wscroll = $(window).scrollTop();
+    if (wscroll > 0) {
+      $('#navbar').addClass("navscroll");
+    } else {
+      $('#navbar').removeClass("navscroll");
+    }
+  });
+});
+//Função que altera a cor do input
+
+function cor(a){
+  if (a == 1) {
+    var NLetras = document.getElementById("txtNome").value;
+  }
+
+
+  if(NLetras == ""){
+    document.getElementById("txtNome").className = "form-golden uk-input";
+  } else if(NLetras.length <= 2){
+    document.getElementById("txtNome").className = "uk-form-danger uk-input";}
+    else {
+  document.getElementById("txtNome").className = "uk-form-success uk-input";
+};
+
+}
